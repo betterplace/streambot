b89211062ae13d3a7015657a6db9037caa5cf81d
Johannes Opper <johannesopper@riseup.net>
2018-05-31 18:10:37 +0200

    Add google fonts capability

--- src/tools/styled.js
+++ src/tools/styled.js
@@ -15,14 +15,30 @@ function extractColorFromParams(name, params, defaultValue) {
   return params.has(name) ? `#${params.get(name)}` : defaultValue
 }
 
+function googleFontsImport(params) {
+  if (params.has('fontFamily')) {
+    const fontFamily = params.get('fontFamily')
+    return `
+      @import url('https://fonts.googleapis.com/css?family=${fontFamily.replace(/ /g, '+')}');
+
+      body {
+        font-family: ${fontFamily.replace(/\+/g, ' ')};
+      }
+    `
+  } else {
+    return 'body { font-family: Verdana; }'
+  }
+}
+
 const StylesFromParams = (props) => {
   return <style type="text/css">{`
+    ${googleFontsImport(props.params)}
+
     body {
       background-color: ${extractColorFromParams('backgroundColor', props.params, 'transparent')};
       color:            ${extractColorFromParams('textColor'      , props.params, 'black')};
       text-align: center;
       font-size: 24px;
-      font-family: Verdana;
     }
     .progressbar {
       background-color: ${extractColorFromParams('progressBackgroundColor', props.params, 'whiteSmoke')};
@@ -33,6 +49,6 @@ const StylesFromParams = (props) => {
       background-color: ${extractColorFromParams('progressColor', props.params, 'green')};
       display: block;
       height: 100%;
-    }
+    };
   `}</style>
 }
