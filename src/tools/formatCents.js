9fa2a65a5efdca51ceb17ee63f6c9e3fd5358ddd
Johannes Opper <johannesopper@riseup.net>
2018-06-01 16:04:20 +0200

    Don't show cents if whole

--- src/tools/formatCents.js
+++ src/tools/formatCents.js
@@ -1,6 +1,11 @@
 export function formatCents(cents) {
   if (cents) {
-    return new Intl.NumberFormat('de-DE', { style: 'currency', currency: 'EUR' }).format(cents / 100)
+    return new Intl.NumberFormat('de-DE', {
+      style: 'currency',
+      currency: 'EUR',
+      minimumFractionDigits: 0,
+      maximumFractionDigits: 2,
+    }).format(cents / 100)
   } else {
     return 'Spende'
   }
