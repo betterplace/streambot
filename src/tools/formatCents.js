04bc6f8624803f7c7ffce6eef2c16926e7c647bf
Johannes Opper <xijo@pm.me>
2018-12-08 14:29:04 +0100

    Improve fraction handling

--- src/tools/formatCents.js
+++ src/tools/formatCents.js
@@ -1,10 +1,11 @@
 export function formatCents(cents) {
   if (cents) {
+    const fraction = (cents % 10 === 0) ? 0 : 2
     return new Intl.NumberFormat('de-DE', {
       style: 'currency',
       currency: 'EUR',
-      minimumFractionDigits: 0,
-      maximumFractionDigits: 2,
+      minimumFractionDigits: fraction,
+      maximumFractionDigits: fraction,
     }).format(cents / 100)
   } else {
     return 'Spende'
