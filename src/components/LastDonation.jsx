2459012e3dbc121f7ab3ab190d29b1ec2cdbc5a6
Johannes Opper <xijo@pm.me>
2020-06-09 10:58:00 +0200

    Add list view for last donation

--- src/components/LastDonation.jsx
+++ src/components/LastDonation.jsx
@@ -4,8 +4,25 @@ import {Nickname, HeadlineWithBr} from '.'
 
 export const LastDonation = (props) => {
   const headline = props.params.get('headline') || 'Letzte Spende:'
-  return <div className='truncate-with-ellipsis'>
-    <HeadlineWithBr content={headline} />
-    <Nickname {...props.data.author} color={props.params.get('nicknameColor')} /><br />{formatCents(props.data.donated_amount_in_cents, props.params)}
-  </div>
+
+  if (props.params.get('list')) {
+    return <div className='truncate-with-ellipsis'>
+      <HeadlineWithBr content={headline} />
+      <List {...props} />
+    </div>
+  } else {
+    return <div className='truncate-with-ellipsis'>
+      <HeadlineWithBr content={headline} />
+      <Nickname {...props.data.author} color={props.params.get('nicknameColor')} /><br />{formatCents(props.data.donated_amount_in_cents, props.params)}
+    </div>
+  }
 }
+
+const List = (props) =>
+  <ul style={{listStyleType: 'none', margin: 0, padding: 0}}>
+    {props.listData.map((data) => (
+      <li key={data.id}>
+        <Nickname {...data.author} color={props.params.get('nicknameColor')} /> {formatCents(data.donated_amount_in_cents, props.params)}
+      </li>
+    ))}
+  </ul>
