bd170b35e61d63900e91e70dbe8094545d52b194
Johannes Opper <xijo@pm.me>
2020-06-10 13:20:33 +0200

    Preload audio, use Audio API directly [#173193307]

--- src/components/DonationAlert.jsx
+++ src/components/DonationAlert.jsx
@@ -10,9 +10,14 @@ export class DonationAlert extends React.Component {
       hidden:    true,
       gif:       params.get('gif'),
       gifHeight: params.get('gifHeight') || 'inherit',
-      mp3:       params.get('mp3'),
       duration:  params.get('duration') || 3,
       wording:   params.get('wording') || 'Neue Spende'
+    }
+
+    if (params.get('mp3')) {
+      this.audioElement = new Audio(params.get('mp3'))
+      this.audioElement.loop = false
+      this.audioElement.load()
     }
   }
 
@@ -21,6 +26,8 @@ export class DonationAlert extends React.Component {
       this.setState({hidden: false})
       if (this.timeout) clearInterval(this.timeout)
       this.timeout = window.setTimeout(() => this.setState({hidden: true}), this.state.duration * 1000)
+
+      if (this.audioElement) this.audioElement.play()
     }
   }
 
@@ -28,7 +35,6 @@ export class DonationAlert extends React.Component {
     if (this.state.hidden) return null
     return <div>
       <Gif src={this.state.gif} height={this.state.gifHeight} />
-      <Mp3 src={this.state.mp3} />
       <br />
       {this.state.wording}
       <br />
@@ -40,9 +46,4 @@ export class DonationAlert extends React.Component {
 const Gif = ({src, height}) => {
   if (!src) return null
   return <img src={src} alt='' height={height} />
-}
-
-const Mp3 = ({src}) => {
-  if (!src) return null
-  return <audio autoPlay><source src={src} type="audio/mpeg" /></audio>
 }
