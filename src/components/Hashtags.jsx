d76a34a10bb78ebd7471028b486585054b732c53
Marco Pagni <hello@marcopagni.com>
2020-06-16 17:27:25 +0200

    Fix [#173358878]

--- src/components/Hashtags.jsx
+++ src/components/Hashtags.jsx
@@ -6,17 +6,21 @@ export const Hashtags = ({data}) => {
   const totalCount = counts.reduce((acc, count) => acc + count)
   const highestCount = Math.max(...counts)
 
-  const rows = Object.entries(data).map(entry => <tr key={entry[0]}>
-    <td className='label-td'>
-      <span>#{entry[0]}</span>
-    </td>
-    <td className='bar-td'>
-      <ProgressBar percentage={highestCount ? (entry[1] * 100 / highestCount) : 0}/>
-    </td>
-    <td className='percentage-td'>
-      <span>{totalCount ? Math.round(entry[1] * 100 / totalCount) : 0}%</span>
-    </td>
-  </tr>)
+  const rows = Object.entries(data).map((entry) => (
+    <tr key={entry[0]}>
+      <td className='label-td'>
+        <span>#{entry[0]}</span>
+      </td>
+      <td className='bar-td'>
+        <ProgressBar
+          data={{ progress_percentage: highestCount ? (entry[1] * 100) / highestCount : 0 }}
+        />
+      </td>
+      <td className='percentage-td'>
+        <span>{totalCount ? Math.round((entry[1] * 100) / totalCount) : 0}%</span>
+      </td>
+    </tr>
+  ))
 
   return <table className='hashtags-table'><tbody>{rows}</tbody></table>
 }
