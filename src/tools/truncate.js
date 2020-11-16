ca0f1c34f4d89afcd273b8cee0e42f94ecf30216
Florian Frank <flori@ping.de>
2020-11-16 19:19:43 +0100

    Fix off-by-one error

--- src/tools/truncate.js
+++ src/tools/truncate.js
@@ -1,4 +1,4 @@
 export const truncate = (string, length) => {
-  if (length && string.length > length) return string.slice(0, length) + '…'
+  if (length && string.length > length) return string.slice(0, length - 1) + '…'
   return string
 }
