af5017886ceb7cf5b126d224cf2175e556c42395
Jannosch Müller <janosch.mueller@betterplace.org>
2021-06-11 11:47:48 +0200

    Comment confusing code

--- src/components/Progress.jsx
+++ src/components/Progress.jsx
@@ -3,13 +3,16 @@ import { formatCents } from '../tools'
 
 export const ProgressBar = (props) => {
   const { data: { progress_percentage } } = props
-  return <div className='progressbar'><span style={{width: `${Math.round(progress_percentage)}%`}}></span></div>
+  return <div className='progressbar'><span style={{ width: `${Math.round(progress_percentage)}%` }}></span></div>
 }
 
 const Collected = ({ params }) => {
   const collected = params.get('collected')
+  // add standard suffix if not overridden or there are zero donations
   if (!collected || 0 === collected.length) return ' gesammelt.'
+  // pass 'false' to disable
   if (collected === 'false') return null
+  // pass another param value to use as alternate suffix
   return collected
 }
 
@@ -18,7 +21,7 @@ const TargetText = (props) => {
     data: { donated_amount_in_cents, requested_amount_in_cents }
   } = props
 
-  return(
+  return (
     <div className='progress-label'>
       {formatCents(donated_amount_in_cents, props.params)} {`von ${formatCents(requested_amount_in_cents, props.params)} `}{Collected(props)}
     </div>
@@ -26,7 +29,7 @@ const TargetText = (props) => {
 }
 
 const TargetProgress = (props) => {
-  switch(props.params.get('display')) {
+  switch (props.params.get('display')) {
     case 'text':
       return <div>
         <TargetText {...props} />
