d7848d7b1f929237002a3b2ab95ff977d99b00d9
Simon Schrape <sim@ilitu.de>
2021-07-13 14:31:31 +0200

    De-dupe basic widgets.

--- src/components/LastDonation.jsx
+++ src/components/LastDonation.jsx
@@ -1,19 +1,4 @@
 import React from 'react'
-import { formatCents } from '../tools'
-import { Nickname, HeadlineWithBr } from '.'
-import { List } from './List'
+import { BasicWidget } from '.'
 
-export const LastDonation = (props) => {
-  const headline = props.params.get('headline') ?? 'Letzte Spende:'
-  const listMode = props.params.get('list') === 'true'
-
-  return <div className='truncate-with-ellipsis' style={{width: props.params.get('width')}}>
-    <HeadlineWithBr content={headline} />
-    {listMode ?
-      <List {...props} simple={true} />
-      : <>
-        <Nickname {...props.data.author} color={props.params.get('nicknameColor')} /><br />{formatCents(props.data.donated_amount_in_cents, props.params)}
-      </>
-    }
-  </div>
-}
+export const LastDonation = (props) => <BasicWidget {...props} defaultHeadline='Letzte Spende:' simpleList />
