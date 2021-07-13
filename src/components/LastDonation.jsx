befdcd3029bd938ecd6f987a6ec00143c11c8e11
Simon Schrape <sim@ilitu.de>
2021-07-13 13:29:33 +0200

    One list to rule them all.

--- src/components/LastDonation.jsx
+++ src/components/LastDonation.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { formatCents } from '../tools'
 import { Nickname, HeadlineWithBr } from '.'
+import { List } from './List'
 
 export const LastDonation = (props) => {
   const headline = props.params.get('headline') ?? 'Letzte Spende:'
@@ -9,19 +10,10 @@ export const LastDonation = (props) => {
   return <div className='truncate-with-ellipsis' style={{width: props.params.get('width')}}>
     <HeadlineWithBr content={headline} />
     {listMode ?
-      <List {...props} />
+      <List {...props} simple={true} />
       : <>
         <Nickname {...props.data.author} color={props.params.get('nicknameColor')} /><br />{formatCents(props.data.donated_amount_in_cents, props.params)}
       </>
     }
   </div>
 }
-
-const List = (props) =>
-  <ul style={{ listStyleType: 'none', margin: 0, padding: 0 }}>
-    {props.listData.map((data) => (
-      <li key={data.id}>
-        <Nickname {...data.author} color={props.params.get('nicknameColor')} /> {formatCents(data.donated_amount_in_cents, props.params)}
-      </li>
-    ))}
-  </ul>
--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import {formatCents,decodeColor} from '../tools'
+import { Nickname } from '.'
+import { formatCents, decodeColor } from '../tools'
 
 const ListNickname = ({ name, color }) => {
   const style = {
@@ -26,6 +27,15 @@ const Amount = ({ amount, params }) => {
 }
 
 export const List = (props) => {
+  // TODO: This is a temporary solution to get the differences out of <LastDonation>. Gotta assimilate both versions.
+  if (props.simple) return <ul style={{ listStyleType: 'none', margin: 0, padding: 0 }}>
+    {props.listData.map((data) => (
+      <li key={data.id}>
+        <Nickname {...data.author} color={props.params.get('nicknameColor')} /> {formatCents(data.donated_amount_in_cents, props.params)}
+      </li>
+    ))}
+  </ul>
+
   return <>
     <link href="https://fonts.googleapis.com/css?family=Roboto+Mono&amp;display=swap" rel="stylesheet" />
     <div>
