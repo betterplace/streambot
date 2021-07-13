7e40233cdfd7dccdc92a677b3d40f27e19ba70d5
Simon Schrape <sim@ilitu.de>
2021-07-13 14:30:14 +0200

    Remove unused param.

--- src/components/HeadlineWithBr.jsx
+++ src/components/HeadlineWithBr.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export const HeadlineWithBr = ({content, visible}) => {
+export const HeadlineWithBr = ({content}) => {
   if (!content || 0 === content.length || content === 'false') return null
   return <React.Fragment>{content}<br/></React.Fragment>
 }
