575cb96ad6a91848fc60388df886bcff99d9e948
Kamil Piwko <iz.podpolja@gmail.com>
2022-09-30 19:31:42 +0200

    guard against any nullish value in formatCents

--- src/tools/formatCents.js
+++ src/tools/formatCents.js
@@ -1,15 +1,20 @@
 export function formatCents(cents, params) {
-  if (cents === undefined) return null
+  if (!cents) return null;
 
-  const currencyDisplay = params.get('currencyDisplay') || 'symbol'
-  let currencyPrecision = params.get('currencyPrecision')
-  currencyPrecision = (currencyPrecision === null) ? ((cents % 100 === 0) ? 0 : 2) : currencyPrecision
+  const currencyDisplay = params.get("currencyDisplay") || "symbol";
+  let currencyPrecision = params.get("currencyPrecision");
+  currencyPrecision =
+    currencyPrecision === null
+      ? cents % 100 === 0
+        ? 0
+        : 2
+      : currencyPrecision;
 
-  return new Intl.NumberFormat('de-DE', {
-    style: 'currency',
-    currency: 'EUR',
+  return new Intl.NumberFormat("de-DE", {
+    style: "currency",
+    currency: "EUR",
     currencyDisplay: currencyDisplay,
     minimumFractionDigits: currencyPrecision,
     maximumFractionDigits: currencyPrecision,
-  }).format(cents / 100)
+  }).format(cents / 100);
 }
