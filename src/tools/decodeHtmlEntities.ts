c6295895f1959bb26b4235894451830a122ec9c2
Katrin Fritz <9885028+katrinundfritz@users.noreply.github.com>
2022-12-13 15:00:36 +0100

    Fix TS issue for decodeHtmlEntities

--- src/tools/decodeHtmlEntities.ts
+++ src/tools/decodeHtmlEntities.ts
@@ -1,5 +1,5 @@
 export function decodeHtmlEntities(inp: any) {
-  var replacements = {
+  var replacements: { [key: string]: string } = {
     '&lt;': '<',
     '&gt;': '>',
     '&sol;': '/',
@@ -14,7 +14,6 @@ export function decodeHtmlEntities(inp: any) {
     '&deg;': '°',
   }
   for (var r in replacements) {
-    // @ts-expect-error TS(7053): Element implicitly has an 'any' type because expre... Remove this comment to see the full error message
     inp = inp.replace(new RegExp(r, 'g'), replacements[r])
   }
   return inp.replace(/&#(\d+);/g, function (match: any, dec: any) {
