450c76169161513396f4a06d583b84533ea21247
Katrin Fritz <9885028+katrinundfritz@users.noreply.github.com>
2022-12-15 14:38:01 +0100

    Improve Hashtag types

--- src/components/Hashtags.tsx
+++ src/components/Hashtags.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import { ProgressBar } from '.'
 
-export const Hashtags = ({ data }: { data: any }) => {
-  const counts: number[] = Object.values(data)
-  const totalCount: number = counts.reduce((acc, count) => acc + count)
+export const Hashtags = ({ data }: { data: Record<string, number> }) => {
+  const counts = Object.values(data)
+  const totalCount = counts.reduce((acc, count) => acc + count)
   const highestCount = Math.max(...counts)
 
-  const entries = Object.entries(data) as [[key: string, value: number]]
+  const entries = Object.entries(data)
 
   const rows = entries.map((entry) => (
     <tr key={entry[0]}>
@@ -14,10 +14,10 @@ export const Hashtags = ({ data }: { data: any }) => {
         <span>#{entry[0]}</span>
       </td>
       <td className="bar-td">
-        <ProgressBar data={{ progress_percentage: highestCount ? ((entry[1] as number) * 100) / highestCount : 0 }} />
+        <ProgressBar data={{ progress_percentage: highestCount ? (entry[1] * 100) / highestCount : 0 }} />
       </td>
       <td className="percentage-td">
-        <span>{totalCount ? Math.round(((entry[1] as number) * 100) / totalCount) : 0}%</span>
+        <span>{totalCount ? Math.round((entry[1] * 100) / totalCount) : 0}%</span>
       </td>
     </tr>
   ))
