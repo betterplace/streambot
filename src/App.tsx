12dc826451aba5e8806e9eda52a095718deb2d26
Katrin Fritz <9885028+katrinundfritz@users.noreply.github.com>
2023-10-17 12:20:37 +0200

    Remove outdated preconfigured widgets and make background image and text color of standard widget adjustable

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import React from 'react'
 import {
-  Betterplay,
   DonationAlert,
   DonationList,
   Hashtags,
   LastComment,
   LastDonation,
-  Mashup,
   Progress,
   ProjectCarrierLogos,
   Standard,
   TopDonation,
   TopDonor,
   Total,
-  Waterstreams,
-  Xmas,
 } from './components'
 import { Route, BrowserRouter as Router } from 'react-router-dom'
 import { reloading, styled } from './tools'
@@ -24,10 +20,6 @@ const App = () => (
     <div>
       <Route exact path="/" render={() => (window.location.href = 'https://github.com/betterplace/streambot')} />
       <Route path="/fundraising-events/:id/standard" component={Standard} />
-      <Route path="/fundraising-events/:id/betterplay" component={Betterplay} />
-      <Route path="/fundraising-events/:id/waterstreams" component={Waterstreams} />
-      <Route path="/fundraising-events/:id/mashup" component={Mashup} />
-      <Route path="/fundraising-events/:id/xmas" component={Xmas} />
       <Route path="/fundraising-events/:id/progress" component={styled(reloading(Progress))} />
       <Route path="/fundraising-events/:id/hashtags" component={styled(reloading(Hashtags))} />
       <Route path="/fundraising-events/:id/last-donation" component={styled(reloading(LastDonation))} />
--- src/components/preconfigured/Betterplay.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import backgroundImage from '../../images/spendenanzeige_betterplay.png'
-import { Standard } from '..'
-import { WidgetProps } from 'components/types'
-
-export const Betterplay = (props: WidgetProps) => (
-  <Standard {...props} backgroundImage={backgroundImage} headlineStyle={{ marginTop: 0 }} />
-)
