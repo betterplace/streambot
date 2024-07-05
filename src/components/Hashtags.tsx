661ed7a8b258594f292162294c2c86aac544c7cd
Kamil Piwko <iz.podpolja@gmail.com>
2024-07-05 11:03:54 +0100

    [DEV-23856] Use amounts instead of % in the Hashtags component

--- src/components/Hashtags.tsx
+++ src/components/Hashtags.tsx
@@ -1,24 +1,23 @@
 import React from 'react'
 import { ProgressBar } from '.'
 import { ReloadingWidgetProps } from './types'
+import { formatCents } from 'tools'
 
 export const Hashtags = (props: ReloadingWidgetProps<Record<string, number>>) => {
-  const counts = Object.values(props.data)
-  const totalCount = counts.reduce((acc, count) => acc + count)
-  const highestCount = Math.max(...counts)
-
   const entries = Object.entries(props.data)
+  const sumOfHashtagAmounts = entries.reduce((acc, [_, amount]) => acc + amount, 0)
+  const highestAmount = Math.max(...entries.map(([_, amount]) => amount))
 
-  const rows = entries.map((entry) => (
-    <tr key={entry[0]}>
+  const rows = entries.map(([hashtag, amountInCents]) => (
+    <tr key={hashtag}>
       <td className="label-td">
-        <span>#{entry[0]}</span>
+        <span>#{hashtag}</span>
       </td>
       <td className="bar-td">
-        <ProgressBar data={{ progress_percentage: highestCount ? (entry[1] * 100) / highestCount : 0 }} />
+        <ProgressBar data={{ progress_percentage: highestAmount ? (amountInCents * 100) / highestAmount : 0 }} />
       </td>
       <td className="percentage-td">
-        <span>{totalCount ? Math.round((entry[1] * 100) / totalCount) : 0}%</span>
+        <span>{sumOfHashtagAmounts && formatCents(amountInCents, new URLSearchParams())}</span>
       </td>
     </tr>
   ))
